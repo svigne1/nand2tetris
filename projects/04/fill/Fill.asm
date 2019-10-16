c76c4507c3843c100596fb23e0a20859b2453b80
vignesh <vignesh>
2019-10-17 00:29:56 +0530

    Project 4 - yet to be  graded

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,65 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// INPUT
+(INFINITE_LOOP)
+@KBD
+D=M;         
+
+// CHECK CONDITIONAL
+@WHITE
+D;JEQ        
+
+// SETS VALUE = 1 & JUMPS TO SET-SCREEN
+(BLACK)
+D=0      
+@VALUE
+M=!D    //  M=1, SETS M AS 000000001. SO, WE NEED TO SET D=000000000 AND USE !D
+@SETSCREEN
+0;JMP
+
+// SETS VALUE = 0 & JUMPS TO SET-SCREEN
+(WHITE)      
+@VALUE
+M=0
+@SETSCREEN
+0;JMP
+
+// SETS SCREEN WITH START VALUE
+(SETSCREEN)
+    // FIRST INDEX IS @SCREEN
+    @SCREEN
+    D=A 
+    @FIRST
+    M=D
+
+    // LAST INDEX IS @KBD
+    @KBD
+    D=A
+    @LAST
+    M=D
+
+    // SET COUNTER TO FIRST & START LOOP
+    @FIRST
+    D=M
+    @POINTER
+    M=D
+
+    // START LOOP
+    (LOOP)
+        @VALUE
+        D=M
+        @POINTER
+        A=M
+        M=D
+        @POINTER
+        M=M+1
+        // SUBTRACT POINTER & LAST TO CHECK IF IT IS 0, WHICH MEANS END HAS BEEN REACHED.
+        D=M
+        @LAST
+        D=D-M
+    @INFINITE_LOOP
+    D;JEQ
+    @LOOP
+    0;JMP
+    
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,41 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// INITIALIZATION
+@COUNTER
+M=0
+@SUM
+M=0
+
+// IF R1 = 0, RETURN
+@R1
+D=M
+@RETURN
+D;JEQ
+
+(LOOP)
+@R0
+D=M
+@SUM
+M=D+M
+@COUNTER
+M=M+1
+D=M
+@R1
+D=D-M;
+@RETURN
+D;JEQ
+@LOOP
+0;JMP
+
+
+(RETURN)
+@SUM
+D=M
+@R2
+M=D
+
+(INFINITE_LOOP)
+@INFINITE_LOOP
+0;JMP
